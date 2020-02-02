c50b84a2265bf092e678ed56c6af7b19f2147548
Ally Jarjour <allyjarjour@gmail.com>
2020-02-02 11:10:22 -0700

    Remove transaction section on page load and create functions for adding/hiding elements when toggling between navigation tabs

--- main.js
+++ main.js
@@ -33,32 +33,68 @@ function toggleDashboard() {
   profileButton.classList.remove("toggle-highlight");
 }
 
-// hide and add elements when toggling nav bar
+// hide elements for transactions page
 
 var main = document.querySelector('.main');
-var table = document.querySelector('.table');
+var table = document.querySelector('.recent-trans-table');
 var tableButton = document.querySelector('.new-transaction-button');
 var tableTitle = document.querySelector('.trans-title');
 var tableSection = document.querySelector('section.recent-transactions');
+var siteTitle = document.querySelector('.site-title');
+var transPageSection = document.querySelector('.transactions-section');
+var transTitle = document.querySelector('.transactions-header');
 
-walletButton.addEventListener("click", hideForTransButton)
+walletButton.addEventListener("click", hideMainElements)
 
-function hideForTransButton () {
+function hideMainElements () {
   main.classList.add('hidden');
   tableButton.classList.add('hidden');
   table.classList.add('hidden');
   tableTitle.classList.add('hidden');
   tableSection.classList.add('hidden');
+  siteTitle.classList.add('hidden');
 }
 
 // Bring back elements when clicking back to Dashboard
 
-dashboardButton.addEventListener("click", addContent)
+dashboardButton.addEventListener("click", addDashElements)
 
-function addContent () {
+function addDashElements () {
   main.classList.remove("hidden");
   tableButton.classList.remove('hidden');
   table.classList.remove('hidden');
   tableTitle.classList.remove('hidden');
   tableSection.classList.remove('hidden');
+  siteTitle.classList.remove('hidden');
 }
+
+// Remove elements for profile page
+
+profileButton.addEventListener("click", removeAllElements)
+
+function removeAllElements() {
+  main.classList.add('hidden');
+  tableButton.classList.add('hidden');
+  table.classList.add('hidden');
+  tableTitle.classList.add('hidden');
+  tableSection.classList.add('hidden');
+  siteTitle.classList.add('hidden');
+  transPageSection.classList.add('hidden');
+  transTitle.classList.add('hidden');
+}
+
+// Bring back elements for transaction page
+
+walletButton.addEventListener("click", addTransContent)
+
+function addTransContent() {
+  transPageSection.classList.remove('hidden');
+  transTitle.classList.remove('hidden');
+}
+
+// Hide transaction content on page toggleDashboard
+
+window.onload = function hideTransOnload() {
+  document.querySelector('.transactions-section').classList.add('hidden');
+  document.querySelector('.transactions-header').classList.add('hidden');
+};
